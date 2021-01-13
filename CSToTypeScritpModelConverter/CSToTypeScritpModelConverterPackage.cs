045f3eca776e1eca08f850aadf8d7d51160d7845
adrian.gajewskiDev@outlook.com <adrian.gajewskiDev@outlook.com>
2021-01-13 15:05:54 +0100

    add convert command

--- CSToTypeScritpModelConverter/CSToTypeScritpModelConverterPackage.cs
+++ CSToTypeScritpModelConverter/CSToTypeScritpModelConverterPackage.cs
@@ -25,6 +25,7 @@ namespace CSToTypeScritpModelConverter
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [Guid(CSToTypeScritpModelConverterPackage.PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
     public sealed class CSToTypeScritpModelConverterPackage : AsyncPackage
     {
         /// <summary>
@@ -46,6 +47,7 @@ namespace CSToTypeScritpModelConverter
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await TestCommand.InitializeAsync(this);
         }
 
         #endregion
